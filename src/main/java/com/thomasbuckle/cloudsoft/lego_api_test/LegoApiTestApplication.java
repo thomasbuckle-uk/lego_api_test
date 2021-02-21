f2868277e1296bd2210b37729b068e3b828efc38
Thomas Buckle <69787238+thomasbuckle-uk@users.noreply.github.com>
2021-02-21 14:49:08 +0000

    Project Start

---
+++ src/main/java/com/thomasbuckle/cloudsoft/lego_api_test/LegoApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.thomasbuckle.cloudsoft.lego_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thomasbuckle/cloudsoft/lego_api_test/LegoApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasbuckle.cloudsoft.lego_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
